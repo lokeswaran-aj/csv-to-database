fdac638b7b7c84e9cbacd5873b0f315bdab102ac
Lokeswaran Aruljothi <11lokeswaran@gmail.com>
2024-01-18 21:02:17 +0530

    Chore: Add error log

--- lib/CSV-to-JSON.ts
+++ lib/CSV-to-JSON.ts
@@ -39,6 +39,7 @@ const getJsonData = async (dataStructure: string, csvString: string) => {
         const jsonResponse = JSON.parse(response);
         return Object.values(jsonResponse)[0];
     } catch (error: any) {
+        console.error(error.message);
         throw new Error("Failed to process CSV data. Please try again.");
     }
 };
